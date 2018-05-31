5a38c7654cf2f0b3fcca7d6b92cab90b289fc71a
ZachC <zacarlin@gmail.com>
2018-05-31 15:57:19 -0400

    -a

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,7 @@
 <template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        pacinis
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
-    </div>
-  </section>
+  <b-containter>
+    <h1>Happy</h1>
+  </b-containter>
 </template>
 
 <script>
